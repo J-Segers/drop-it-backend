9f5e0750fa52186dc92c04a5d4ba5958d60c41ba
J-Segers <jethroseger@gmail.com>
2022-06-06 13:31:00 +0200

    initial commit

---
+++ src/main/java/com/dropit/backend_drop_it/BackendDropItApplication.java
@@ -0,0 +1,13 @@
+package com.dropit.backend_drop_it;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDropItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendDropItApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dropit/backend_drop_it/BackendDropItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dropit.backend_drop_it;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDropItApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
